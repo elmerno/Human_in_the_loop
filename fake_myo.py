a869cb1b6a8709d5cf32e87c0143b42d84c14f8b
elmerno <33283427+elmerno@users.noreply.github.com>
2018-03-01 16:17:46 +0100

    Add files via upload

--- fake_myo.py
+++ fake_myo.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+from geometry_msgs.msg import Vector3
     
 
 def fakeMyo():
     rospy.init_node('robot_controller', anonymous=True)
     gest = rospy.Publisher("myo_raw/myo_gest_str", String, queue_size=100)
-    ori = rospy.Publisher("myo_raw/myo_ori_deg", String, queue_size=100)
+    ori = rospy.Publisher("myo_raw/myo_ori_deg", Vector3, queue_size=100)
     gest_rate = rospy.Rate(0.4)
     while not rospy.is_shutdown():
-        ori.publish("x: 3.2525252\ny: -65.3424242\nz: 5.51251525")
+        ori.publish(Vector3(3.2525252, -65.3424242, 5.51251525))
         gest.publish("FINGERS_SPREAD")
         gest_rate.sleep()
 
